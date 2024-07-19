ee96040be50a1d9d827900d45094ad83a6711a5e
Matheus Rosa <matheusrosataxa@gmail.com>
2024-07-18 22:09:36 -0300

    feat: crud finalizado

--- app/main.py
+++ app/main.py
@@ -1,30 +1,42 @@
+from multiprocessing import Value
 from fastapi import FastAPI
-
-from app.models import Music, MusicLibrary
+from random import choice
+from app.models import Music, MusicInDB, MusicLibrary
 
 app = FastAPI(title="Music API", description="API for music", version="1.0.0")
 
 
-@app.post("/songs", response_model=dict | str)
+@app.post("/songs", response_model=dict | str, status_code=201)
 def create_song(music: Music):
     return MusicLibrary.create(music)
 
 
-@app.get("/songs")
-def get_songs(): ...
+@app.get("/songs", response_model=list[MusicInDB])
+def get_songs():
+    return MusicLibrary.read()
 
 
-@app.get("/songs/{song_id}")
-def get_song_by_id(song_id: int): ...
+@app.get("/songs/{song_id}", response_model=MusicInDB)
+def get_song_by_id(song_id: str):
+    return MusicLibrary.read_one(song_id)
 
 
-@app.get("/songs/random")
-def get_random_song(): ...
+@app.get("/songs/random", response_model=MusicInDB)
+def get_random_song():
+    return choice(MusicLibrary.read())
 
 
-@app.put("/songs/{song_id}")
-def update_song(song_id: int): ...
+@app.put("/songs/{song_id}", response_model=MusicInDB)
+def update_song(song_id: str):
+    try:
+        return MusicLibrary.update(song_id)
+    except ValueError:
+        return "Música não encontrada"
 
 
-@app.delete("/songs/{song_id}")
-def delete_song(song_id: int): ...
+@app.delete("/songs/{song_id}", response_model=dict | str)
+def delete_song(song_id: int):
+    try:
+        return MusicLibrary.delete(song_id)
+    except ValueError:
+        return "Música não encontrada"
--- app/models.py
+++ app/models.py
@@ -1,5 +1,10 @@
+from multiprocessing import Value
+import re
+from bson import ObjectId
 from pydantic import BaseModel, Field
+from pymongo import ReturnDocument
 from app.database import db
+from app.main import update_song
 
 
 class Music(BaseModel):
@@ -11,7 +16,9 @@ class Music(BaseModel):
     image: str
 
 
-class MusicInDB(Music):
+class MusicInDB(
+    Music
+):  # Essa classe existe para manuear as musicas no banco dados, pois no MongoDB o id é retornado como um ObjectId (que trata-se de uma instancia) e não como id normal
     id: str = Field(..., alias="_id")  # Facilitar o uso do id do banco de dados
 
 
@@ -22,3 +29,41 @@ class MusicLibrary:
     def create(cls, music: Music):
         cls._collections.insert_one(music.__dict__)  # ou vars(music)
         return "Música cadastrada com sucesso"
+
+    @classmethod
+    def read(cls):
+        for song in cls._collection.find():
+            MusicInDB(
+                _id=str(song.pop("_id")), **song
+            )  # coleta o id do banco de dados, remove o id e retorna o id removido, então temos em mão o id do banco de dados e esse deve ser passado como string para o MusicInDB, além do restante dos dados (que nao precisam de nenhum tratamento)
+            # [MusicInDB(_id=str(song.pop("_id")), **song) for song in cls._collection.find()] # outra forma de fazer a mesma coisa
+
+    @classmethod
+    def read_one(cls, song_id: str):
+        found_song = cls._collection.find_one(
+            {"_id": ObjectId(song_id)}
+        )  # tem que procurar pelo id utilizando o _id, pois é assim que o MongoDB ele é armazenado como protegido
+        if found_song is not None:
+            return MusicInDB(
+                _id=str(found_song.pop("_id")), **found_song
+            )  # CONVERSAO DO ID PARA PODER ADICIONAR O VALOR DO ID AO NOSSO ATRIBUTO DA CLASSE MusicInDB
+        else:
+            raise ValueError("Música não encontrada")
+
+    @classmethod
+    def update(cls, song_id: str, music: Music):
+        updated_song = cls._collection.find_one_and_update(  # o find_one_and_update permite que você atualize um documento e retorne o documento antes ou depois da atualização
+            {"_id": ObjectId(song_id)},
+            {"$set": music.__dict__},
+            return_document=ReturnDocument.AFTER,
+        )
+        if updated_song is None:
+            raise ValueError("Música não encontrada")
+        return MusicInDB(_id=str(update_song.pop("_id")), **update_song)
+
+    @classmethod
+    def delete(cls, song_id: str):
+        deleted_song = cls._collection.find_one_and_delete({"_id": ObjectId(song_id)})
+        if deleted_song is None:
+            raise ValueError("Música não encontrada")
+        return "Música deletada com sucesso"
